df1ae60fdabca91e58cd95a7d2be43a1ab82e3f3
Daniel Vergeylen <daniel.vergeylen@place2be.biz>
2020-04-14 14:08:19 +0200

    fix: complied to ESLint rules 😊

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,47 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <div>
+    <p>Testing green background</p>
+  </div>
+
+  <div>
+    <button class="button is-link">
+      <i class="fas fa-sign-in"></i>
+      Click Me!
+    </button>
+  </div>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style lang='scss'>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  div {
+    p {
+      background: green;
+      color: white;
+    }
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
