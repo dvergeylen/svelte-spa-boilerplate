8584b3a7fc2333d89caf706f629c98d8c4e726a1
Daniel Vergeylen <daniel.vergeylen@place2be.biz>
2020-04-14 13:26:28 +0200

    reference: add Bulma
    Main configuration is in the Rollup config file. It bundles a new file:
    bundle.js which contains all libraries stuff (NO component stuff).

---
+++ src/javascripts/bundle.js
@@ -0,0 +1,8 @@
+/* This file contains the libraries to be included in ALL html
+ *  * e.g: bulma, fontawsome, etc
+ *   * NO COMPONENTS here
+ *    */
+
+/* SCSS */
+/* These will be put in bundle.css by rollup */
+import '../stylesheets/bulma.scss';
