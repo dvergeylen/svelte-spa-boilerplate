6138c2040c2f27dcd931ac7e93f429736e08c220
Daniel Vergeylen <daniel.vergeylen@place2be.biz>
2020-04-14 13:37:17 +0200

    reference: add Bulma (continued)
    Added missing configuration file 😕

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,99 @@
-import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+
+// Plugins
+import scss from 'rollup-plugin-scss';
+import svelte from 'rollup-plugin-svelte';
 
 // Pre-processes
 import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			},
-      preprocess: sveltePreprocess(),
-		}),
+export default [
+  {
+	  input: 'src/javascripts/bundle.js',
+	  output: {
+	  	sourcemap: true,
+	  	format: 'iife',
+	  	name: 'app',
+	  	file: 'public/build/bundle.js'
+	  },
+	  plugins: [
+      scss({
+        output: 'public/build/bundle.css',
+        failOnError: true,
+      }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
+	  	// If you have external dependencies installed from
+	  	// npm, you'll most likely need these plugins. In
+	  	// some cases you'll need additional configuration -
+	  	// consult the documentation for details:
+	  	// https://github.com/rollup/plugins/tree/master/packages/commonjs
+	  	resolve({
+	  		browser: true,
+	  		dedupe: ['svelte']
+	  	}),
+	  	commonjs(),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+	  	// In dev mode, call `npm run start` once
+	  	// the bundle has been generated
+	  	!production && serve(),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+	  	// Watch the `public` directory and refresh the
+	  	// browser on changes when not in production
+	  	!production && livereload('public'),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
-};
+	  	// If we're building for production (npm run build
+	  	// instead of npm run dev), minify
+	  	production && terser()
+	  ],
+	  watch: {
+	  	clearScreen: false
+	  }
+  },
+  {
+	  input: 'src/main.js',
+	  output: {
+	  	sourcemap: true,
+	  	format: 'iife',
+	  	name: 'app',
+	  	file: 'public/build/main.js'
+	  },
+	  plugins: [
+	  	svelte({
+	  		// enable run-time checks when not in production
+	  		dev: !production,
+	  		// we'll extract any component CSS out into
+	  		// a separate file - better for performance
+	  		css: css => {
+	  			css.write('public/build/main.css');
+	  		},
+        preprocess: sveltePreprocess(),
+	  	}),
+
+	  	// If you have external dependencies installed from
+	  	// npm, you'll most likely need these plugins. In
+	  	// some cases you'll need additional configuration -
+	  	// consult the documentation for details:
+	  	// https://github.com/rollup/plugins/tree/master/packages/commonjs
+	  	resolve({
+	  		browser: true,
+	  		dedupe: ['svelte']
+	  	}),
+	  	commonjs(),
+
+	  	// If we're building for production (npm run build
+	  	// instead of npm run dev), minify
+	  	production && terser()
+	  ],
+	  watch: {
+	  	clearScreen: false
+	  }
+  }
+];
 
 function serve() {
 	let started = false;
